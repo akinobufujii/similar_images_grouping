812f2471833411c97c11e7194ac217b6facd5a69
akinobufujii <aki.develop@gmail.com>
2022-04-29 15:57:01 +0900

    feat #--- readimageutil追加

---
+++ readimageutil/readimageutil.go
@@ -0,0 +1,66 @@
+package readimageutil
+
+import (
+	"image"
+	"image/jpeg"
+	"image/png"
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+)
+
+// ReadImage 画像データ読み込み（内部で拡張子による分岐を行う）
+func ReadImage(path string) (image.Image, error) {
+	ext := filepath.Ext(path)
+	switch ext {
+	case ".jpg", ".jpeg":
+		imageData, err := ReadJpeg(path)
+		if err != nil {
+			return imageData, errors.Wrap(err, "failed ReadJpeg")
+		}
+		return imageData, nil
+
+	case ".png":
+		imageData, err := ReadPng(path)
+		if err != nil {
+			return imageData, errors.Wrap(err, "failed ReadPng")
+		}
+		return imageData, nil
+	}
+
+	var imageData image.Image
+	return imageData, errors.New("unknown image " + ext)
+}
+
+// ReadJpeg jpeg画像読み込み
+func ReadJpeg(path string) (image.Image, error) {
+	var imageData image.Image
+	file, err := os.Open(path)
+	if err != nil {
+		return imageData, errors.Wrap(err, "failed os.Open")
+	}
+
+	imageData, err = jpeg.Decode(file)
+	if err != nil {
+		return imageData, errors.Wrap(err, "failed jpeg.Decode")
+	}
+
+	return imageData, nil
+}
+
+// ReadPng png画像読み込み
+func ReadPng(path string) (image.Image, error) {
+	var imageData image.Image
+	file, err := os.Open(path)
+	if err != nil {
+		return imageData, errors.Wrap(err, "failed os.Open")
+	}
+
+	imageData, err = png.Decode(file)
+	if err != nil {
+		return imageData, errors.Wrap(err, "failed png.Decode")
+	}
+
+	return imageData, nil
+}

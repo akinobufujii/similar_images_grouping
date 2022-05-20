efa07267d306c47536f1e6b744d9bdf4dc52473b
akinobufujii <aki.develop@gmail.com>
2022-05-20 23:12:47 +0900

    ImageHashInfoのエンコード・デコード処理追加

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"archive/zip"
+	"bufio"
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -24,6 +26,60 @@ import (
 type ImageHashInfo struct {
 	Filepath  string
 	ImageHash *goimagehash.ExtImageHash
+}
+
+// EncodeJson Jsonデータにエンコード
+func (p *ImageHashInfo) EncodeJson(enc *json.Encoder) error {
+	b := bytes.Buffer{}
+	writer := bufio.NewWriter(&b)
+	if err := p.ImageHash.Dump(writer); err != nil {
+		return errors.Wrap(err, "failed ImageHash.Dump")
+	}
+
+	if err := writer.Flush(); err != nil {
+		return errors.Wrap(err, "failed Flush")
+	}
+
+	encodeData := struct {
+		Filepath      string
+		ImageHashDump string
+	}{
+		Filepath:      p.Filepath,
+		ImageHashDump: base64.StdEncoding.EncodeToString(b.Bytes()),
+	}
+
+	if err := enc.Encode(encodeData); err != nil {
+		return errors.Wrap(err, "failed Encode")
+	}
+
+	return nil
+}
+
+// DecodeJson Jsonデータにデコード
+func (p *ImageHashInfo) DecodeJson(dec *json.Decoder) error {
+	decodeData := struct {
+		Filepath      string
+		ImageHashDump string
+	}{}
+
+	err := dec.Decode(&decodeData)
+	if err != nil {
+		return errors.Wrap(err, "failed Decode")
+	}
+
+	data, err := base64.StdEncoding.DecodeString(decodeData.ImageHashDump)
+	if err != nil {
+		return errors.Wrap(err, "failed DecodeString")
+	}
+
+	reader := bufio.NewReader(bytes.NewBuffer(data))
+	p.ImageHash, err = goimagehash.LoadExtImageHash(reader)
+	if err != nil {
+		return errors.Wrap(err, "failed LoadExtImageHash")
+	}
+	p.Filepath = decodeData.Filepath
+
+	return nil
 }
 
 // streamSendWalkFilepath 指定ディレクトリ以下のwalk結果を返していくストリーム
--- main_test.go
+++ main_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"math/bits"
+	"os"
+	"reflect"
 	"testing"
 
 	"github.com/akinobufujii/similar_images_grouping/readimageutil"
@@ -30,6 +33,58 @@ func TestImageHash(t *testing.T) {
 	t.Logf("filetype: %v\n", imageType)
 	t.Logf("hash: %v\n", imagehash.ToString())
 	t.Logf("onesCount: %v\n", onesCount)
+}
+
+// TestEncodeDecodeImageHashInfo エンコード・デコードテスト
+func TestEncodeDecodeImageHashInfo(t *testing.T) {
+	path := "samples/Cerberus_Front_Pres_01.jpg"
+	imageData, _, err := readimageutil.ReadImage(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// NOTE: pHashを計算
+	imagehash, err := goimagehash.ExtPerceptionHash(imageData, 16, 16)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// NOTE: エンコード・デコード
+	imageHashInfoA := ImageHashInfo{Filepath: path, ImageHash: imagehash}
+	imageHashInfoB := ImageHashInfo{}
+
+	t.Logf("imageHashInfoA.filename: %v\n", imageHashInfoA.Filepath)
+	t.Logf("imageHashInfoA.hash: %v\n", imageHashInfoA.ImageHash.ToString())
+
+	saveFile := "imagehash_temp.json"
+	file, err := os.Create(saveFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	jsonEncoder := json.NewEncoder(file)
+	jsonEncoder.SetIndent("", "  ")
+	if err := imageHashInfoA.EncodeJson(jsonEncoder); err != nil {
+		t.Fatal(err)
+	}
+
+	file.Close()
+
+	file, err = os.Open(saveFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	jsonDecoder := json.NewDecoder(file)
+	if err := imageHashInfoB.DecodeJson(jsonDecoder); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("imageHashInfoB.filename: %v\n", imageHashInfoB.Filepath)
+	t.Logf("imageHashInfoB.hash: %v\n", imageHashInfoB.ImageHash.ToString())
+
+	if !reflect.DeepEqual(imageHashInfoA, imageHashInfoB) {
+		t.Fatal("failed encode/decode imageHashInfo")
+	}
 }
 
 func TestWriteJson(t *testing.T) {
